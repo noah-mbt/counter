c7fc001749e9eaadf15bda5897a6b61c0bf2f9e8
Noah Mbutcho <noahmbutcho@gmail.com>
2023-08-07 14:31:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const decreaseBtn = document.querySelector("#decrease")
+const resetBtn = document.querySelector("#reset")
+const increaseBtn = document.querySelector("#increase")
+
+
+function reset () {
+    
+}
