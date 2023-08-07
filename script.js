5bcf440572d3c97b49845dcfe8e952f96f6955fc
Noah Mbutcho <noahmbutcho@gmail.com>
2023-08-07 19:06:40 +0200

    add functionality to buttons

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const decreaseBtn = document.querySelector("#decrease")
-const resetBtn = document.querySelector("#reset")
-const increaseBtn = document.querySelector("#increase")
+const decreaseBtn = document.querySelector("#decrease");
+const resetBtn = document.querySelector("#reset");
+const increaseBtn = document.querySelector("#increase");
+const display = document.querySelector("#display");
 
+
+decreaseBtn.addEventListener("click", decrease);
+resetBtn.addEventListener("click", reset);
+increaseBtn.addEventListener("click", increase);
+
+function decrease () {
+    display.textContent -= 1;
+};
 
 function reset () {
-    
-}
+    display.textContent = 0;
+};
+
+function increase () {
+    let displayInt = parseInt(display.textContent);
+    display.textContent = displayInt + 1;
+};
+
+
